3229d66d3f7d8ce1c59227cb1ac42b58a6d09337
Rizal_Niomic <ananahnu12345@gmail.com>
2020-10-15 08:13:15 +0800

    Tugas 13

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import DaftarBuah from './Tugas-10/DaftarBuah';
+// import DaftarBuah from './Tugas-10/DaftarBuah';
 // import Timer from './Tugas-11/timer';
 // import Clock from './Tugas-11/waktu';
-import FormBuah from './Tugas-9/FormBuah';
-import List from './Tugas-12/list';
+// import FormBuah from './Tugas-9/FormBuah';
+// import List from './Tugas-12/list';
+import Example from './Tugas-13/tugas13';
 
 function App() {
   return (
 
     <div>
-      <FormBuah/>
-      <DaftarBuah/>
+      {/* <FormBuah/> */}
+      {/* <DaftarBuah/> */}
       {/* <Timer start="100"/> */}
       {/* <Clock/> */}
-      <List/>
+      {/* <List/> */}
+      <Example/>
     </div>
     
 
